014a9c37a0809d87265a8238aa6b22856055c820
T47 <t47@stanford.edu>
2015-01-18 20:08:04 -0800

    restore admin

--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import *
 from settings import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', 'rmdb.views.home'),
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     # (r'^admin/', include(admin.site.urls)),
+     (r'^admin/', include(admin.site.urls)),
 )
