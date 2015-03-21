1a0d29f273910019a1ec4f45632724e82614fa2c
T47 <t47@stanford.edu>
2015-03-20 19:09:42 -0700

    remove repository/deprecated

--- repository/deprecated/rmdb_nightly_backup.sh
+++
@@ -1,8 +0,0 @@
-fname=rmdb_`date +%m-%d-%Y`
-localname=/home/daslab/rmdb_backup/
-remotename=/home/tsuname/rmdb_backup/
-mysqldump -u root -pbeckman rmdb | gzip > $localname$fname.sql.gz
-echo "scp -r $localname$fname.sql.gz tsuname@ade.stanford.edu:$remotename$fname"
-scp $localname$fname.sql.gz tsuname@ade.stanford.edu:$remotename
-sudo tar zcf $localname${fname}_site.gz /home/daslab/rdat/rmdb
-scp $localname${fname}_site.gz tsuname@ade.stanford.edu:$remotename
