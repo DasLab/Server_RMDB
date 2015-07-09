5c972bb5ff58af11372f2859e4730878f8825d6b
T47 <t47@stanford.edu>
2015-07-08 21:46:39 -0700

    change csv permission

--- util_chmod.sh
+++ util_chmod.sh
@@ -5,12 +5,14 @@ sudo chown -R www-data data
 sudo chmod 640 *.py* *.md *.txt
 sudo chmod 640 repository/*.py* repository/helper/* repository/templatetags/*
 sudo chmod 750 repository repository/templatetags
-sudo chmod 640 misc/*.txt misc/V*.* misc/external/* misc/mapseeker/* misc/reeffit/*
-sudo chmod 750 misc misc/external misc/mapseeker misc/reeffit
 sudo chmod 640 media/css/* media/fonts/* media/html/* media/js/* media/images/*.*g* media/images/*.*p* media/images/docs/* media/rss/*
 sudo chmod 750 media/css media/fonts media/html media/js media/images media/images/docs media/rss media
 sudo chmod 640 media/admin/*.html media/admin/css/* media/admin/img/*.*g* media/admin/img/gis/* media/admin/img/admin/* media/admin/js/*.js media/admin/js/*.txt media/admin/js/*.py* media/admin/js/admin/* 
 sudo chmod 750 media/admin media/admin/css media/admin/img media/admin/img/gis media/admin/img/admin media/admin/js media/admin/js/admin 
+
+sudo chmod 640 misc/*.txt misc/V*.* misc/external/* misc/mapseeker/* misc/reeffit/*
+sudo chmod 750 misc misc/external misc/mapseeker misc/reeffit
+sudo chown www-data misc/mapseeker/mapseeker_user.csv misc/reeffit/reeffit_user.csv 
 
 sudo chmod -R 640 data/
 sudo chmod 750 data data/construct_img data/files data/thumbs
