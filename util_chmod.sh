3837dbdffb2e175d1b834ad7a7766afed7b68e35
Ubuntu <ubuntu@rmdb.stanford.edu>
2016-03-06 06:53:36 +0000

    use xargs for chmod

--- util_chmod.sh
+++ util_chmod.sh
@@ -6,23 +6,23 @@ sudo chown -R www-data backup data cache
 sudo chmod 640 *.py* robots.txt .gitignore
 sudo chmod 600 *.md requirements.txt
 
-sudo chmod 640 $(find src -type f)
-sudo chmod 750 $(find src -type d)
-sudo chmod 640 $(find media -type f)
-sudo chmod 750 $(find media -type d)
-sudo chmod 640 $(find misc -type f)
-sudo chmod 750 $(find misc -type d)
+find src -type f | sudo xargs chmod 640
+find src -type d | sudo xargs chmod 750
+find media -type f | sudo xargs chmod 640
+find media -type d | sudo xargs chmod 750
+find misc -type f | sudo xargs chmod 640
+find misc -type d | sudo xargs chmod 750
 
-sudo chmod 660 $(find cache -type f)
-sudo chmod 770 $(find cache -type d)
-sudo chmod 660 $(find backup -type f)
-sudo chmod 770 $(find backup -type d)
-sudo chmod 660 $(find data -type f)
-sudo chmod 770 $(find data -type d)
+find cache -type f | sudo xargs chmod 660
+find cache -type d | sudo xargs chmod 770
+find backup -type f | sudo xargs chmod 660
+find backup -type d | sudo xargs chmod 770
+find data -type f | sudo xargs chmod 660
+find data -type d | sudo xargs chmod 770
 
-sudo chmod 640 $(find config -type f)
-sudo chmod 750 $(find config -type d)
-sudo chown www-data config/cron.conf 
+find config -type f | sudo xargs chmod 640
+find config -type d | sudo xargs chmod 750
+sudo chown www-data config/cron.conf
 
 sudo chown -R ubuntu:ubuntu *.sh
 sudo chmod -R 700 *.sh
