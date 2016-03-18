f2fee5445f20f7e47891112bee5d4247df5e774d
T47 <t47@stanford.edu>
2016-03-17 21:27:56 -0700

    src_dist sh chmod

--- util_src_dist.sh
+++ util_src_dist.sh
@@ -1,5 +1,7 @@
 cd ~/RDAT_Kit/rdatkit
-sudo chown ubuntu:ubuntu *.pyc
+sudo chown ubuntu:ubuntu *.py*
 cd ..
-git pull
+sudo git pull
 sudo python setup.py install
+cd ~/RDAT_Kit/rdatkit
+sudo chown ubuntu:ubuntu *.py*
